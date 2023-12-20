88047a516e834c4d09aae18cbf1e2a5d4cb0d604
Peng Jiang <16434640+pengjiang80@users.noreply.github.com>
2023-12-20 20:15:38 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ variable "instance_type" {
 variable "ami_id" {
   description = "The ID of the AMI to use for the EC2 instance"
   type        = string
-  default     = "ami-0c94855ba95c71c99"
+  default     = "ami-07c589821f2b353aa"
 }
 
 variable "subnet_id" {
   description = "The ID of the subnet to launch the EC2 instance in"
   type        = string
-  default     = "subnet-0123456789abcdef0"
+  default     = "subnet-01136e2eaaa0cfcb5"
 }
 
 variable "security_group_ids" {
   description = "A list of security group IDs to associate with the EC2 instance"
   type        = list(string)
-  default     = ["sg-0123456789abcdef0"]
+  default     = ["sg-06ec672f8d33c1464"]
 }
 
 resource "aws_instance" "ec2_instance" {
